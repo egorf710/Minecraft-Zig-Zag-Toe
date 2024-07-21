959f12ea3ed0f52c7e2ca5814194e7399dfbe1f4
egorf710 <egorf710@gmail.com>
2024-07-22 02:23:48 +0300

    123

--- Assets/ZigZag2D/Scripts/GameManager.cs
+++ Assets/ZigZag2D/Scripts/GameManager.cs
@@ -235,6 +235,7 @@ namespace TunnelGame
 						FindAnyObjectByType<AdsReward>().OpenFullScrAd();
 						ad = true;
 						YandexGame.NewLeaderboardScores("Leaders", (int)HighScore);
+						print("upd lbdr");
 					}
 				}
 
